d60c690d1342d3476fc29e50d3cf3e489a185f67
WalterResende85 <42590216+WalterResende85@users.noreply.github.com>
2020-01-06 18:51:42 -0300

    api

--- src/main/java/com/stefanini/concurso/resource/ConcursoCandidatoController.java
+++ src/main/java/com/stefanini/concurso/resource/ConcursoCandidatoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stefanini.concurso.DTO.ConcursoCandidatoDTO;
@@ -19,6 +20,7 @@ import com.stefanini.concurso.model.ConcursoCandidato;
 import com.stefanini.concurso.service.ConcursoCandidatoService;
 
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/concursoCandidato")
 @RestController
 public class ConcursoCandidatoController {
 
